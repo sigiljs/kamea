c413f2eca2684775db4221ab05a710db7300c506
Richard <richard.anaya@gmail.com>
2016-12-15 21:25:14 -0600

    adding time travel

--- kamea.js
+++ kamea.js
@@ -8,17 +8,17 @@ function Domain(name,store){
 	this._commit = this._commit.bind(this);
 }
 
-Domain.prototype._commit = function(fn){
+Domain.prototype._commit = function(action,fn){
 	this._store.state[this._name].transact();
 	fn(this._store.state[this._name],this._store.state);
 	this._store.state[this._name].run();
-	this._store._commit();
+	this._store._commit({type:"$$ASYNC:"+action.type});
 }
 
 Domain.prototype.dispatch = function(action){
   if(this._actions[action.type]){
 		this._store.state[this._name].transact();
-    this._actions[action.type](this._store.state[this._name],action,this._commit,this._store.state);
+    this._actions[action.type](this._store.state[this._name],action,(fn)=>this._commit(action,fn),this._store.state);
 		this._store.state[this._name].run();
   }
 }
@@ -32,6 +32,28 @@ function Kamea(){
   this.state = this._freezer.get();
   this._domains = {};
   this._subscribers = [];
+
+	if(window && window.__REDUX_DEVTOOLS_EXTENSION__){
+    this._devTools = window.__REDUX_DEVTOOLS_EXTENSION__.connect();
+    this._devTools.subscribe(this._onDevToolMessage.bind(this));
+  }
+}
+
+Kamea.prototype._onDevToolMessage = function(message){
+	if (message.type === 'START') {
+    this._devToolStarted = true;
+    var state = this.getState();
+    this._devTools.init(state)
+  } else if (message.type === 'STOP') {
+    this._devToolStarted = false;
+  } else if (message.type === 'DISPATCH' && message.payload.type === "JUMP_TO_STATE") { // Received a store action from Dispatch monitor
+    this.setState(JSON.parse(message.state));
+  }
+}
+
+Kamea.prototype._notifyDevToolActionState = function(action,state){
+	if (!this._devToolStarted) return;
+	this._devTools.send(action, state);
 }
 
 Kamea.prototype._publishNewState = function(newState){
@@ -44,10 +66,11 @@ Kamea.prototype._publishNewState = function(newState){
 	}
 }
 
-Kamea.prototype._commit = function(){
+Kamea.prototype._commit = function(action){
 	this.state.run();
 	var newState = this._freezer.get();
 	if(this.state != newState){
+		this._notifyDevToolActionState(action,newState);
 		this._publishNewState(newState);
 	}
 }
@@ -66,8 +89,12 @@ Kamea.prototype.domain = function(name,initialState){
 Kamea.prototype.dispatch = function(action){
 	for(var i in this._domains){
   	this._domains[i].dispatch(action);
-		this._commit();
+		this._commit(action);
   }
+}
+
+Kamea.prototype.getState = function(){
+	return this._freezer.get();
 }
 
 Kamea.prototype.setState = function(state){
